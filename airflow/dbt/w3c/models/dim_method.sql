870dfc8fc5030c859e043995b10356484b3b2005
Ahmed Ikram <2571642@dundee.ac.uk>
2026-05-28 02:40:31 +0100

    Add dim_method model for dbt integration to process HTTP methods and their characteristics

---
+++ airflow/dbt/w3c/models/dim_method.sql
@@ -0,0 +1,36 @@
+{{ config(
+    materialized='table',
+    tags=['dimension', 'dbt']
+) }}
+
+WITH raw_methods AS (
+    SELECT DISTINCT
+        COALESCE(NULLIF(TRIM(UPPER(method)), ''), 'Unknown') AS method_name
+    FROM {{ source('w3c', 'raw_logs') }}
+    WHERE method IS NOT NULL
+),
+
+method_entries AS (
+    SELECT
+        ROW_NUMBER() OVER (ORDER BY rm.method_name) AS method_sk,
+        rm.method_name AS http_method,
+        CASE
+            WHEN rm.method_name = 'GET' THEN 'Retrieve a resource'
+            WHEN rm.method_name = 'POST' THEN 'Submit data to the server'
+            WHEN rm.method_name = 'PUT' THEN 'Upload a resource'
+            WHEN rm.method_name = 'DELETE' THEN 'Delete a resource'
+            WHEN rm.method_name = 'HEAD' THEN 'Retrieve headers only'
+            WHEN rm.method_name = 'OPTIONS' THEN 'Describe communication options'
+            WHEN rm.method_name = 'PATCH' THEN 'Partial resource update'
+            ELSE 'Other HTTP Method'
+        END AS description,
+        CASE
+            WHEN rm.method_name IN ('GET', 'HEAD', 'OPTIONS') THEN 'Safe'
+            WHEN rm.method_name IN ('POST', 'PUT', 'PATCH', 'DELETE') THEN 'Unsafe'
+            ELSE 'Unknown'
+        END AS is_safe
+    FROM raw_methods rm
+)
+
+SELECT * FROM method_entries
+ORDER BY method_sk
