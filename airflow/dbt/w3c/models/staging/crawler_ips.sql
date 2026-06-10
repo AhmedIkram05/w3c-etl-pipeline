e62e632e1ff87ed5c551fdfabf3d55bf3b1ef40f
Ahmed Ikram <2571642@dundee.ac.uk>
2026-06-10 13:12:26 +0100

    fix: enhance T-SQL compatibility in `crawler_ips.sql`
    - Added conditional logic to the WHERE clause for `is_crawler` to support both SQL Server and other databases.
    - Ensured consistent boolean handling across different database types.

--- airflow/dbt/w3c/models/staging/crawler_ips.sql
+++ airflow/dbt/w3c/models/staging/crawler_ips.sql
@@ -2,4 +2,8 @@
 
 SELECT DISTINCT client_ip AS ip
 FROM {{ source('w3c', 'raw_enriched') }}
+{% if target.type == 'sqlserver' %}
+WHERE is_crawler = 1
+{% else %}
 WHERE is_crawler = TRUE
+{% endif %}
